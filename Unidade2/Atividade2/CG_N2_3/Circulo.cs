38e595de8a132e96fe7edf9ed8958d4cde9b706f
Natália Sens Weise <nataliaweise@gmail.com>
2023-04-22 12:20:31 -0300

    Activity 2.3: Increase and decrease radius value

---
+++ Unidade2/Atividade2/CG_N2_3/Circulo.cs
@@ -0,0 +1,31 @@
+#define CG_Debug // debugar texto.
+using CG_Biblioteca;
+using OpenTK.Graphics.OpenGL4;
+
+namespace gcgcg
+{
+    internal class Circulo : Objeto
+    {
+        public Circulo(Objeto paiRef, double tam) : base(paiRef)
+        {
+            PrimitivaTipo = PrimitiveType.Points;
+            Ponto4D pto = new Ponto4D();
+            for (int i=0; i<360; i+=5){
+                pto = Matematica.GerarPtosCirculo(i, tam);
+                base.PontosAdicionar(pto);
+            }
+            base.ObjetoAtualizar();        
+        }
+
+        #if CG_Debug
+        public override string ToString()
+        {
+        string retorno;
+        retorno = "__ Objeto Retangulo _ Tipo: " + PrimitivaTipo + "\n";
+        retorno += base.ToString();
+        return (retorno);
+
+        }
+        #endif
+    }
+}
