522e039376e47c9306acf770e9786576570aad35
Natália Sens Weise <nataliaweise@gmail.com>
2023-05-11 08:39:03 -0300

    Activity 2.4: Spline is visible

--- Unidade2/Atividade2/CG_N2_4/Spline.cs
+++ Unidade2/Atividade2/CG_N2_4/Spline.cs
@@ -8,44 +8,49 @@ namespace gcgcg
 {
   internal class Spline : Objeto
   {
-
-    //precisa adicionar paiRef será?
-    private Ponto4D ptoA;
-    private Ponto4D ptoB;
-    private Ponto4D ptoC;
-    private Ponto4D ptoD;
     public int qntPtos;
     public List<Ponto4D> ptosSpline = new List<Ponto4D>();
 
     public Spline(Objeto paiRef, Ponto4D ptoA, Ponto4D ptoB, Ponto4D ptoC, Ponto4D ptoD, int qntPtos) : base(paiRef)
     {
-      this.ptoA = ptoA;
-      this.ptoB = ptoB;
-      this.ptoC = ptoC;
-      this.ptoD = ptoD;
+      PrimitivaTipo = PrimitiveType.LineLoop;
+      PrimitivaTamanho = 1;
+      base.PontosAdicionar(ptoA);
+      base.PontosAdicionar(ptoB);
+      base.PontosAdicionar(ptoC);
+      base.PontosAdicionar(ptoD);
       this.qntPtos = qntPtos;
 
+      Atualizar();
       DrawSpline();
     }
+    public void Atualizar()
+    {
 
-    public void DrawSpline(){
+      base.ObjetoAtualizar();
+    }
+
+    public void DrawSpline()
+    {
       for (int i = 0; i < qntPtos; i++){
-        Ponto4D ptoAB = Calcular(ptoA, ptoB);
-        Ponto4D ptoBC = Calcular(ptoB, ptoC);
-        Ponto4D ptoCD = Calcular(ptoC, ptoD);
+        Ponto4D ptoAB = Calcular(this.PontosId(0), this.PontosId(1));
+        Ponto4D ptoBC = Calcular(this.PontosId(1), this.PontosId(2));
+        Ponto4D ptoCD = Calcular(this.PontosId(2), this.PontosId(3));
         Ponto4D ptoX = Calcular(ptoAB, ptoBC);
         Ponto4D ptoY = Calcular(ptoBC, ptoCD);
         Ponto4D resultado = Calcular(ptoX, ptoY);
+        Atualizar();
         ptosSpline.Add(resultado);
       }
     }
 
-    private Ponto4D Calcular(Ponto4D pto, Ponto4D pto1){
+    private Ponto4D Calcular(Ponto4D pto, Ponto4D pto1)
+    {
       double X = pto.X + (pto1.X - pto.X);
       double Y = pto.Y + (pto1.Y - pto.Y);
 
       return new Ponto4D(X, Y);
-      }
+    }
 
   }
 }
